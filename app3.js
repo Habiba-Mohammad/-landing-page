94a664cc9a5f3228e2306ab624068049d010de4d
Habiba-Mohammad <88598540+Habiba-Mohammad@users.noreply.github.com>
2022-03-09 00:51:58 +0200

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,123 @@
+/*
+// get elements from Html 
+let Nav = document.getElementById('navbar__list');
+let sections = document.querySelectorAll('section');
+// change the color of the title  
+let style=document.querySelector('h1').style.color=('red');
+let classLink=document.getElementsByClassName("menu__link");
+// set the varibales of the project 
+let link  =""
+let contaner= `<li><ul><a class="menu__link" id="classId" href="`;
+let counter=1;
+let sectionId="#section";
+let scrolling2="<style>html{scroll-behavior: smooth;}</style>"
+let list=[]
+// first requirement add a dynamic navigation bar // 
+for (let section=1;section<=sections.length;section+=1){
+    let Id = sectionId+counter;
+    let DataNav = 'Section '+section;
+// add counter instead of section because counter start from 1 but section start from zero 
+    counter++;
+    link += `${contaner}${Id}">${DataNav}</a></ul><li>`;
+    list=link
+
+///// change color of section when scrolling using addEventListener and getBoundingClientRect()
+let sectionScroll=document.getElementById('section'+section);
+let x= document.querySelector("section")
+let section3=section;
+section3-=1;
+activeClass=document.getElementsByClassName("your-active-class")
+document.addEventListener("scroll",function(){
+  if(sectionScroll.getBoundingClientRect().top>=-400&&
+  sectionScroll.getBoundingClientRect().top<=150){
+  if(sectionScroll.classList.contains('class'))
+    sectionScroll.classList.add("your-active-class")
+   sectionScroll.style.backgroundColor="green"
+   }
+else{sectionScroll.classList.remove("your-active-class")
+sectionScroll.style.backgroundColor="pink"
+}})}
+/////////////////////////////////////////////////////////////////////
+Nav.innerHTML = link;
+// smooth scrolling 
+//document.head.insertAdjacentHTML("beforeend",scrolling2);
+// text replace by js 
+
+let textReplace = document.getElementById("section1").innerHTML;
+document.getElementById("section1").innerHTML = textReplace.replace("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero", "I am speech and language pathologist and web developer............................................");
+////////////////////////////////////
+*/
+// get elements from Html
+
+let Nav = document.getElementById("navbar__list");
+let sections = document.querySelectorAll("section");
+
+let style = (document.querySelector("h1").style.color = "red");
+
+let classLink = document.getElementsByClassName("menu__link");
+
+// set the varibales of the project
+
+let link = "";
+
+let contaner = `<li class="menu__link" id="classId"`;
+
+let counter = 1;
+
+let sectionId = "#section";
+
+let scrolling2 = "<style>html{scroll-behavior: smooth;}</style>";
+
+let list = [];
+
+// first requirement add a dynamic navigation bar //
+
+for (let section = 1; section <= sections.length; section += 1) {
+  let Id = sectionId + counter;
+
+  let DataNav = "Section " + section;
+
+  // add counter instead of section because counter start from 1 but section start from zero
+  console.log(DataNav);
+  counter++;
+  function clickFun(section) {
+    sections[section - 1].scrollIntoView({ behavior: "smooth" });
+  }
+  link += `${contaner}" style="display:inline-block" onclick="clickFun(${section})"}>${DataNav}</li>`;
+
+  list = link;
+
+  ///// change color of section when scrolling using addEventListener and getBoundingClientRect()
+
+  let sectionScroll = document.getElementById("section" + section);
+
+  let x = document.querySelector("section");
+
+  let section3 = section;
+
+  section3 -= 1;
+
+  activeClass = document.getElementsByClassName("your-active-class");
+
+  document.addEventListener("scroll", function () {
+    if (
+      sectionScroll.getBoundingClientRect().top >= -20 &&
+      sectionScroll.getBoundingClientRect().top <= 150
+    ) {
+      sectionScroll.classList.add("your-active-class");
+
+      sectionScroll.style.backgroundColor = "green";
+    } else {
+      sectionScroll.classList.remove("your-active-class");
+      sectionScroll.style.backgroundColor = "transparent";
+      //sectionScroll.style.backgroundColor="pink"
+    }
+  });
+}
+
+/////////////////////////////////////////////////////////////////////
+
+Nav.innerHTML = link;
+
+
+////////////////////////////////////
